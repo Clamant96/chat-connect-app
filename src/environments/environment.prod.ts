136aa0eb3fb798ebef88e56c47996052fd0c985d
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2022-05-05 18:19:06 -0300

    Atualizacao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  //server: "http://localhost",
-  //port: ":8080",
-  server: "http://192.168.15.150",
+  server: "http://localhost",
   port: ":8080",
   id: 0,
 	username: "",
